502d810d4c58787b6b5dcbfaa045bf5512467b72
adjknowles <alastair_knowles@yahoo.co.uk>
2015-10-13 18:05:20 +0100

    Re-ordered functions in the javascript

--- js/app.js
+++ js/app.js
@@ -22,43 +22,29 @@ window.onload = function(){
     $('#instruct').toggle();
   }
 
-  function chooseAnswer(){
-    var answer = $(this).text();
+  function updateTimer(){
+    $('#start').removeClass("animated infinite pulse")
+    $('#timer').addClass("animated infinite pulse")
+    var counter = 31;
+    play = true;
 
-    $(".answers").html("")
+    $("#instruct").hide();
 
-    if (answer.toString() === res.toString()) {
-      $(".guessing").removeClass("guessing").addClass("correct");
-      playerScore++;
-      question.html("")
-    } else {
-      $(".guessing").removeClass("guessing");
-    };
-
-    if ($(".correct").length === 16) {
-      clearInterval(A);
-      $("#question").hide();
-      $(".grid").hide();
-      $('#timer').removeClass("animated infinite pulse")
-      $('#newgame').addClass("animated infinite pulse")
-      return  $('.game-over').html("Well Done!" + "<br/>" + "You Scored The Maximum " + "<br/>" + playerScore + " Points!");
-    }
-  }
-
-  function generateRandomAnswer(correct){
-    var answer; 
-    if (Math.random() > 0.5) {
-      answer = Math.floor(correct - (Math.random()*10));
-    } else {
-      answer = Math.floor(correct + (Math.random()*10));
-    }
-    if (correct === answer) return false;
-    return answer;
-  }
-
-  function shuffleAnswers(o){
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-      return o;
+    A = setInterval(function(){
+      counter --;
+      if(counter >= 0){
+        return timer.html(counter);
+      } else {
+        $("#question").hide();
+        $(".answers").hide();
+        $('#timer').removeClass("animated infinite pulse")
+        timer.html("0");
+        $(".grid").hide();
+        $('#newgame').addClass("animated infinite pulse")
+        return $('.game-over').html("GAME OVER!" + "<br/>" + "You Scored " + playerScore + " Points");
+        play = false;
+      }
+    }, 1000);
   }
 
   function getEquation(){
@@ -90,7 +76,6 @@ window.onload = function(){
     
     res = parseFloat(res.toFixed(2));
 
-    // result.innerHTML = res;
     question.html("<span id='what-is'>What is:</span><br> " + rnum1 + " " + op + " " + rnum2 + " ?");
 
     var answers = [res];
@@ -109,42 +94,46 @@ window.onload = function(){
       '</button>' + '<button class="answer">'+parseFloat(myArray[2].toFixed(2))+'</button>');
   }
 
-  function updateTimer(){
-    $('#start').removeClass("animated infinite pulse")
-    $('#timer').addClass("animated infinite pulse")
-    var counter = 31;
-    play = true;
+  function generateRandomAnswer(correct){
+    var answer; 
+    if (Math.random() > 0.5) {
+      answer = Math.floor(correct - (Math.random()*10));
+    } else {
+      answer = Math.floor(correct + (Math.random()*10));
+    }
+    if (correct === answer) return false;
+    return answer;
+  }
 
-    $("#instruct").hide();
+  function shuffleAnswers(o){
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+      return o;
+  }
 
-    A = setInterval(function(){
-      counter --;
-      if(counter >= 0){
-        return timer.html(counter);
-      } else {
-        $("#question").hide();
-        $(".answers").hide();
-        $('#timer').removeClass("animated infinite pulse")
-        timer.html("0");
-        $(".grid").hide();
-        $('#newgame').addClass("animated infinite pulse")
-        return $('.game-over').html("GAME OVER!" + "<br/>" + "You Scored " + playerScore + " Points");
-        play = false;
-      }
-    }, 1000);
+  function chooseAnswer(){
+    var answer = $(this).text();
+
+    $(".answers").html("")
+
+    if (answer.toString() === res.toString()) {
+      $(".guessing").removeClass("guessing").addClass("correct");
+      playerScore++;
+      question.html("")
+    } else {
+      $(".guessing").removeClass("guessing");
+    };
+
+    if ($(".correct").length === 16) {
+      clearInterval(A);
+      $("#question").hide();
+      $(".grid").hide();
+      $('#timer').removeClass("animated infinite pulse")
+      $('#newgame').addClass("animated infinite pulse")
+      return  $('.game-over').html("Well Done!" + "<br/>" + "You Scored The Maximum " + "<br/>" + playerScore + " Points!");
+    }
   }
 
   function newGame(){
     setTimeout(location.reload(true), 5000)
-    // playerScore = 0
-    // res = 0;
-    // play = false;
-    // var $lis = $("li")
-    // $.each($lis, function(index, li){
-    //   $(li).removeClass("correct");
-    // });
-    // $(".grid").show();
-    // $('#score').html("");
-    // updateTimer();
   }
 }
